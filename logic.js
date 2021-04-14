a6dcfd51852249597f9c9408752c3b170f4bae77
Utkarsh Seksaria <utkarshseksaria@gmail.comexitexit()exitgit config --global user.email utkarshseksaria@gmail.com>
2021-04-14 16:50:12 +0530

    Add js code

---
+++ logic.js
@@ -0,0 +1,88 @@
+  'use strict'
+var buttons = document.getElementsByTagName("button");
+var display = document.getElementById("display");
+// console.log(pres);
+// pres[0].addEventListener('click',function(event){
+//   console.log('key is pressed and code is',event.keyCode);
+// })
+var operand1=0;
+var operand2;
+var operator=null;
+
+
+for( var i=0; i<buttons.length; i++)
+{
+    buttons[i].addEventListener('click', function(){
+      var value = this.getAttribute('data-value');
+      // console.log(value);
+      if(value=='clear') display.innerText=null;
+      else if(value=='+')
+      {
+        operand1 = display.innerText;
+        display.innerText=null;
+        operator='+';
+      }
+      else if(value=='-')
+      {
+        operand1 = display.innerText;
+        display.innerText=null;
+        operator='-';
+      }
+      else if(value=='*')
+      {
+        operand1 = display.innerText;
+        display.innerText=null;
+        operator='*';
+      }
+      else if(value=='/')
+      {
+        operand1 = display.innerText;
+        display.innerText=null;
+        operator='/';
+      }
+      else if(value=='sign')
+      {
+        var s='-';
+        var num = parseInt(display.innerText);
+        num = (-num)
+        display.innerText = num;
+      }
+      else if(value=='=')
+      {
+          operand2 = display.innerText;
+          if(operator=='+')
+          {
+            display.innerText= parseInt(operand1) + parseInt(operand2);
+            operand1= operand2;
+            operand2=null;
+            operator=null;
+          }
+          else if(operator=='-')
+          {
+            display.innerText= parseInt(operand1) - parseInt(operand2);
+            operand1= operand2;
+            operand2=null;
+            operator=null;
+          }
+          else if(operator=='*')
+          {     
+            display.innerText= parseInt(operand1) * parseInt(operand2);
+            operand1= operand2;
+            operand2=null;
+            operator=null;
+          }
+          else if(operator=='/')
+          {
+            display.innerText = parseInt(operand1) / parseInt(operand2);
+            operand1=operand2;
+            operator=null;
+            operand2=null;
+          }
+      }
+      else 
+      {
+        display.innerText += value;
+      }
+    })
+}
+console.log('value');
